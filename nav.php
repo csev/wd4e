eead3aba5f165cb412dc22449d08d969c75e7430
Charles Severance <csev@umich.edu>
2026-08-27 09:41:37 -0400

    Fix fatal error.

--- nav.php
+++ nav.php
@@ -1,4 +1,16 @@
 <?php
+
+if ( ! defined('COOKIE_SESSION') ) {
+    define('COOKIE_SESSION', true);
+}
+
+if ( ! isset($CFG) ) {
+    require_once __DIR__ . '/tsugi/config.php';
+}
+if ( session_id() == "" ) {
+    $LAUNCH = \Tsugi\Core\LTIX::session_start();
+}
+
 $OUTPUT->bodyStart();
 $R = $CFG->apphome . '/';
 $T = $CFG->wwwroot . '/';
